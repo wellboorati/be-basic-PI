563bf03fe2222d565f9a57ed1fd6a6fc2f648115
leticiajesussilva <leticiajesussilva@gmail.com>
2022-08-04 15:15:29 -0300

    rota listarprodutos funcionando

--- controllers/produtosEstoqueController.js
+++ controllers/produtosEstoqueController.js
@@ -39,7 +39,10 @@ const produtosEstoqueController = {
     return res.render("produtosestoque");//ou res.json
   },
 
-  
+  listarProdutos: async (req,res)=>{
+    const produtos = await database.Produto_estoque.findAll()
+    return res.json(produtos);
+  }
 
 };
 
